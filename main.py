a6cc86b05c617a36fbee11431b42534124b784d2
Hugo MERCIER <hugo.mercier@ipilyon.net>
2020-11-04 14:57:45 +0100

    Update main.py
    add rollTheDice function

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template
 import sqlite3
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+#roll the dice and return the result. parameters: d = dice rolled (4,6,8,10,100), nbD = number of dice rolled
+def rollTheDice(d,nbD=1):
+    rnd=0
+    for i in range(0,nbD) :
+        rnd+=random.randint(1,d)
+    return(str(rnd))
