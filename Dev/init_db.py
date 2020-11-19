9f364183edba1d30606b175ce08f67ebd466fd2f
Laverone <kilian.averone@ipilyon.net>
2020-11-19 19:19:09 +0100

    Add folder Dev

---
+++ Dev/init_db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect('LogRPG.db')
+cur = conn.cursor()
+
+req = "CREATE TABLE LogRPG_Account(LogRPG_Account_Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, LogRPG_Account_Login TEXT(50) NOT NULL, LogRPG_Account_Password TEXT NOT NULL);"
+cur.execute(req)
+conn.commit()
+
+req = "CREATE TABLE LogRPG_Senario(LogRPG_Senario_Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, LogRPG_Senario_Title TEXT NOT NULL, LogRPG_Senario_Text TEXT, LogRPG_Senario_Account_Id INTEGER NOT NULL, CONSTRAINT fk_LogRPG_Senario_LogRPG_Account_1 FOREIGN KEY (LogRPG_Senario_Account_Id) REFERENCES LogRPG_Account(LogRPG_Account_Id));"
+cur.execute(req)
+conn.commit()
+
+req = "CREATE TABLE LogRPG_Character(LogRPG_Character_Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, LogRPG_Character_Name TEXT(255) NOT NULL, LogRPG_Character_Sheet TEXT, LogRPG_Character_Inventory TEXT, LogRPG_Character_Senario_Id INTEGER NOT NULL, CONSTRAINT fk_LogRPG_Character_LogRPG_Senario_1 FOREIGN KEY (LogRPG_Character_Senario_ID) REFERENCES LogRPG_Senario(LogRPG_Senario_Id));"
+cur.execute(req)
+conn.commit()
+
+conn.close
